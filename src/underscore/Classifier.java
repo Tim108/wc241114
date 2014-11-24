16845ce1b5a8f364fc47410f6a4ab01c895c5478
Martijn <m.j.willemsen@student.utwente.nl>
2014-11-24 10:54:18 +0100

    Created probabilitycounter and shizzle

--- src/underscore/Classifier.java
+++ src/underscore/Classifier.java
@@ -14,6 +14,25 @@ public class Classifier {
 		return data.replaceAll("["+SetUp.punctuation+"]", "").toLowerCase().split(" ");
 	}
 	
+	public double p (String w, SetUp.GENDER given){
+		double freq = given.getFrequentcy(w, given);
+		int TotNum = given.getList(given).size();
+		return freq/TotNum;
+	}
+	
+	public double overallP (String[] words, SetUp.GENDER given){
+		double res = -1;
+		for(String word: words){
+			if (res == -1){
+				res = p(word, given);
+			}
+			else {
+				res=res*p(word, given);
+			}
+		}
+		return Math.log(res)/Math.log(2);
+	}
+	
 	public static void main (String[] args){
 		Classifier c = new Classifier();
 		System.out.println(Arrays.toString(c.extract("hoi Ddit is een toiasf98 Q# Q)M FH#QJ Q(# (FQ FQ#*)R Q#FQ(_JFQ)*VH qhroqnr98 q0wjf h9qhfqja fc1h0qiofejq 9wgr39h 9f7gq3r q9 estje?")));
--- src/underscore/SetUp.java
+++ src/underscore/SetUp.java
@@ -1,7 +1,27 @@
 package underscore;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class SetUp {
-	public static enum GENDER {MALE, FEMALE};
-	//public static Map<Integer, LinkedList<String>> = new HashMap<Integer, LinkedList<String>>();
-	public static final String punctuation = "(){},.;!?<>%"+"'\"*#_";
+	public static enum GENDER {
+		MALE, FEMALE;
+		public static HashMap<String, Integer> maleList = new HashMap<String, Integer>();
+		public static HashMap<String, Integer> femaleList = new HashMap<String, Integer>();
+		
+		public HashMap<String, Integer> getList(GENDER gen) {
+			if (gen.equals(MALE))
+				return maleList;
+			else
+				return femaleList;
+		}
+		
+		public double getFrequentcy(String word, GENDER gen) {
+			return getList(gen).get(word)/getList(gen).size();
+		}
+	};
+
+	// public static Map<Integer, LinkedList<String>> = new HashMap<Integer,
+	// LinkedList<String>>();
+	public static final String punctuation = "(){},.;!?<>%" + "'\"*#_";
 }
