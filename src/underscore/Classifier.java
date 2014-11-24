0bd9438e53902a38811e50a624c4e2fc55a34f1e
Martijn <m.j.willemsen@student.utwente.nl>
2014-11-24 10:04:28 +0100

    tokenisation geupdate: punctiation toegevoegd

--- src/underscore/Classifier.java
+++ src/underscore/Classifier.java
@@ -4,6 +4,7 @@ public class Classifier {
 	
 	public String[] extract (String data){
 		data.toLowerCase();
+		data.replaceAll(SetUp.punctuation, "");
 		return data.split(" ");
 	}
 }
--- src/underscore/SetUp.java
+++ src/underscore/SetUp.java
@@ -2,5 +2,6 @@ package underscore;
 
 public class SetUp {
 	public static enum GENDER {MALE, FEMALE};
-	public static Map<Integer, LinkedList<String>> = new HashMap<Integer, LinkedList<String>>();
+	//public static Map<Integer, LinkedList<String>> = new HashMap<Integer, LinkedList<String>>();
+	public static final String punctuation = ".,?'\"!()";
 }
