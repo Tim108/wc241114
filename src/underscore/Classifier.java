d9431a2b415d0e48b7560956cd77ed4f605b079f
Martijn <m.j.willemsen@student.utwente.nl>
2014-11-24 10:24:54 +0100

    Tokenization af

--- src/underscore/Classifier.java
+++ src/underscore/Classifier.java
@@ -1,10 +1,21 @@
 package underscore;
 
+import java.util.Arrays;
+
 public class Classifier {
 	
+	/**
+	 * 2.1 tokenization
+	 * This method will strip any punctiation that is mentioned in the setup file and split the sentence into seperate words.
+	 * @param data the sentence
+	 * @return an array of words without punctuation.
+	 */
 	public String[] extract (String data){
-		data.toLowerCase();
-		data.replaceAll(SetUp.punctuation, "");
-		return data.split(" ");
+		return data.replaceAll("["+SetUp.punctuation+"]", "").toLowerCase().split(" ");
+	}
+	
+	public static void main (String[] args){
+		Classifier c = new Classifier();
+		System.out.println(Arrays.toString(c.extract("hoi Ddit is een toiasf98 Q# Q)M FH#QJ Q(# (FQ FQ#*)R Q#FQ(_JFQ)*VH qhroqnr98 q0wjf h9qhfqja fc1h0qiofejq 9wgr39h 9f7gq3r q9 estje?")));
 	}
 }
--- src/underscore/SetUp.java
+++ src/underscore/SetUp.java
@@ -3,5 +3,5 @@ package underscore;
 public class SetUp {
 	public static enum GENDER {MALE, FEMALE};
 	//public static Map<Integer, LinkedList<String>> = new HashMap<Integer, LinkedList<String>>();
-	public static final String punctuation = ".,?'\"!()";
+	public static final String punctuation = "(){},.;!?<>%"+"'\"*#_";
 }
