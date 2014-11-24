5e3bf2254871e0ddfed3838f307142e2402d56fa
Martijn <m.j.willemsen@student.utwente.nl>
2014-11-24 11:30:16 +0100

    2.2.1

--- src/underscore/Classifier.java
+++ src/underscore/Classifier.java
@@ -3,53 +3,63 @@ package underscore;
 import java.util.Arrays;
 
 public class Classifier {
-	
+
 	/**
-	 * 2.1 tokenization
-	 * This method will strip any punctiation that is mentioned in the setup file and split the sentence into seperate words.
-	 * @param data the sentence
+	 * 2.1 tokenization This method will strip any punctiation that is mentioned
+	 * in the setup file and split the sentence into seperate words.
+	 * 
+	 * @param data
+	 *            the sentence
 	 * @return an array of words without punctuation.
 	 */
-	public String[] extract (String data){
-		return data.replaceAll("["+SetUp.punctuation+"]", "").toLowerCase().split(" ");
+	public String[] extract(String data) {
+		return data.replaceAll("[" + SetUp.punctuation + "]", "").toLowerCase()
+				.split(" ");
 	}
-	
+
 	/**
 	 * 2.2.1
+	 * 
 	 * @param w
 	 * @param given
 	 * @return
 	 */
-	public double p (String w, SetUp.GENDER given){
-		double freq = given.getFrequentcy(w, given);
-		int TotNum = given.getList().size();
-		return freq/TotNum;
+	public double p(String w, SetUp.GENDER given) {
+		if (given.getList().containsKey(w)) {
+			double freq = given.getFrequentcy(w, given);
+			int TotNum = given.getList().size();
+			double res = freq / TotNum;
+			return res;
+		} else {
+			return 1;
+		}
 	}
-	
+
 	/**
 	 * 2.2.1
+	 * 
 	 * @param words
 	 * @param given
 	 * @return
 	 */
-	public double overallP (String[] words, SetUp.GENDER given){
+	public double overallP(String[] words, SetUp.GENDER given) {
 		double res = -1;
-		for(String word: words){
-			if (res == -1){
+		for (String word : words) {
+			if (res == -1) {
 				res = p(word, given);
-			}
-			else {
-				res=res*p(word, given);
+			} else {
+				res = res * p(word, given);
 			}
 		}
-		return Math.log(res)/Math.log(2);
+		double resLog = Math.log(res) / Math.log(2);
+		return resLog;
 	}
-	
-	public static void insert (String word, int number, SetUp.GENDER gender){
+
+	public static void insert(String word, double number, SetUp.GENDER gender) {
 		gender.getList().put(word, number);
 	}
-	
-	public static void main (String[] args){
+
+	public static void main(String[] args) {
 		Classifier c = new Classifier();
 		insert("male", 5, SetUp.GENDER.MALE);
 		insert("dude", 2, SetUp.GENDER.MALE);
@@ -59,7 +69,9 @@ public class Classifier {
 		insert("ladies", 2, SetUp.GENDER.FEMALE);
 		insert("love", 5, SetUp.GENDER.FEMALE);
 		insert("i", 15, SetUp.GENDER.FEMALE);
-		System.out.println(c.overallP(c.extract("Yo Dude, my male henk is fucking herman"),SetUp.GENDER.MALE));
-		System.out.println(Arrays.toString(c.extract("hoi Ddit is een toiasf98 Q# Q)M FH#QJ Q(# (FQ FQ#*)R Q#FQ(_JFQ)*VH qhroqnr98 q0wjf h9qhfqja fc1h0qiofejq 9wgr39h 9f7gq3r q9 estje?")));
+		System.out.println(c.overallP(
+				c.extract("Yo Dude, my male henk is fucking herman"),
+				SetUp.GENDER.MALE));
+		// System.out.println(Arrays.toString(c.extract("hoi Ddit is een toiasf98 Q# Q)M FH#QJ Q(# (FQ FQ#*)R Q#FQ(_JFQ)*VH qhroqnr98 q0wjf h9qhfqja fc1h0qiofejq 9wgr39h 9f7gq3r q9 estje?")));
 	}
 }
--- src/underscore/SetUp.java
+++ src/underscore/SetUp.java
@@ -1,15 +1,14 @@
 package underscore;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 
 public class SetUp {
 	public static enum GENDER {
 		MALE, FEMALE;
-		public static HashMap<String, Integer> maleList = new HashMap<String, Integer>();
-		public static HashMap<String, Integer> femaleList = new HashMap<String, Integer>();
+		public static HashMap<String, Double> maleList = new HashMap<String, Double>();
+		public static HashMap<String, Double> femaleList = new HashMap<String, Double>();
 		
-		public HashMap<String, Integer> getList() {
+		public HashMap<String, Double> getList() {
 			if (this.equals(MALE))
 				return maleList;
 			else
@@ -17,11 +16,7 @@ public class SetUp {
 		}
 		
 		public double getFrequentcy(String word, GENDER gen) {
-			System.out.println(getList().size());
-			return getList().
-					get(word)/
-					getList().
-					size();
+			return (getList().get(word)/getList().size());
 		}
 	};
 
