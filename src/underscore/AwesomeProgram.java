64f3e93bdd506adc3efe01775cbbba8e167d0944
Tim108 <tsonderen108@gmail.com>
2014-12-02 11:07:06 +0100

    no message

--- src/underscore/AwesomeProgram.java
+++ src/underscore/AwesomeProgram.java
@@ -17,11 +17,14 @@ public class AwesomeProgram {
 	private JFrame frame2;
 	private JPanel panel;
 	private JLabel l1;
-	private JLabel l2;
+	private JLabel l2 = new JLabel("Initializing");
 	private JTextArea a1;
 	private JButton b1;
 	private JButton b2;
 	private JButton b3;
+	
+	private String lastTxt = "";
+	private SetUp.GENDER lastGender;
 
 	/**
 	 * Launch the application.
@@ -30,10 +33,8 @@ public class AwesomeProgram {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					AwesomeProgram window = new AwesomeProgram(true);
-					window.frame.setVisible(true);
-					AwesomeProgram window2 = new AwesomeProgram(false);
-					window2.frame2.setVisible(true);
+					AwesomeProgram window = new AwesomeProgram();
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -44,48 +45,81 @@ public class AwesomeProgram {
 	/**
 	 * Create the application.
 	 */
-	public AwesomeProgram(boolean in) {
-		initialize(in);
+	public AwesomeProgram() {
+		initializeIn();
 	}
+	
+	private void initializeIn(){
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize(boolean in) {
-		if(in){
-			//input frame
-			frame = new JFrame();
-			frame.setBounds(100, 100, 400, 300);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setLayout(new BorderLayout());
-			l1 = new JLabel("Enter a sentence here");
-			frame.add(l1, BorderLayout.NORTH);
-			a1 = new JTextArea(400,200);
-			frame.add(a1, BorderLayout.CENTER);
-			b1 = new JButton("Confirm");
-			frame.add(b1, BorderLayout.SOUTH);
-			b1.addActionListener(new ActionListener() {
-				 
-	            public void actionPerformed(ActionEvent e)
-	            {
-	            	String txt = a1.getText();
-	            	System.out.println(txt);
-	            	c.checkGender(txt);
-	            }
-	        });      
-	 
-		}else{
-			//output frame
-			frame2 = new JFrame();
-			frame2.setBounds(100, 100, 400, 300);
-			frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame2.setLayout(new GridLayout(3,1));
-			l2 = new JLabel("This is not manly");
-			frame2.add(l2);
-			b2 = new JButton("Das ok");
-			frame2.add(b2);
-			b3 = new JButton("Das bullshit");
-			frame2.add(b3);
-		}
+		//input frame
+		frame = new JFrame();
+		frame.setBounds(100, 100, 400, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		l1 = new JLabel("Enter a sentence here");
+		frame.add(l1, BorderLayout.NORTH);
+		a1 = new JTextArea(400,200);
+		frame.add(a1, BorderLayout.CENTER);
+		b1 = new JButton("Confirm");
+		frame.add(b1, BorderLayout.SOUTH);
+		b1.addActionListener(new ActionListener() {
+			 
+            public void actionPerformed(ActionEvent e)
+            {	
+            	l2.setText("banaantjes");
+            	String lastTxt = a1.getText();
+            	lastGender = c.checkGender(lastTxt);
+            	
+            	if(lastGender == SetUp.GENDER.MALE){
+            		System.out.println("1");
+            		initializeOut("That is a male sentence.");
+            	}else if(lastGender == SetUp.GENDER.FEMALE){
+            		System.out.println("2");
+            		initializeOut("That is a female sentence.");
+            	}else{
+            		System.out.println("3");
+            		initializeOut("No gender found");
+            	}
+            }
+        });      
+		frame.setVisible(true);
+	}
+	
+	private void initializeOut(String txt){
+		//output frame
+		frame2 = new JFrame();
+		frame2.setBounds(100, 100, 400, 300);
+		frame2.setLayout(new GridLayout(3,1));
+		l2 = new JLabel(txt);
+		frame2.add(l2);
+		b2 = new JButton("Das ok");
+		b2.addActionListener(new ActionListener() {
+			 
+            public void actionPerformed(ActionEvent e)
+            {	
+            	if(lastGender != null){
+            	//program was right
+            	SetUp.processTrainingData(lastTxt, lastGender);
+            	System.out.println("ok");
+            	frame2.dispose();
+            	}
+            }
+        }); 
+		frame2.add(b2);
+		b3 = new JButton("Das bullshit");
+		b3.addActionListener(new ActionListener() {
+			 
+            public void actionPerformed(ActionEvent e)
+            {
+            	if(lastGender != null){
+            	//program was not right
+            	SetUp.processTrainingData(lastTxt, lastGender.other());
+            	System.out.println("not ok");
+            	frame2.dispose();
+            	}
+            }
+        }); 
+		frame2.add(b3);
+		frame2.setVisible(true);
 	}
 }
